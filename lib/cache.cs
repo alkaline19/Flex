34c1799c3a9cb1d298468e41d4db43a71a37fc34
WindingTheRopes <17016045+WindingTheRopes@users.noreply.github.com>
2020-02-21 19:50:29 -0500

    update

--- lib/cache.cs
+++ lib/cache.cs
@@ -2,7 +2,7 @@
 
 namespace flexlib
 {
-    public class Class1
+    public class cache
     {
     }
 }
---
+++ lib/global.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Flex.flexlib
+{
+    class global
+    {
+        public static string version = "0.0.1 BETA";
+        public static string release = "beta";
+    }
+    
+}

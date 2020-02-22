6d7c97561538cc6d9c336a75530ccb6817664039
alkaline19 <alkaline19@users.noreply.github.com>
2020-02-21 21:21:57 -0500

    Update global.cs
    Jason add comments and make class public.

--- lib/global.cs
+++ lib/global.cs
@@ -2,9 +2,9 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace Flex.flexlib
+namespace Flex.flexlib //make sure your usings references this namespace.
 {
-    class global
+    public class global
     {
         public static string version = "0.0.1 BETA";
         public static string release = "beta";
